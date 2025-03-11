e95830350b351a0b9defdc67871b6222eddce653
Bhavya Saini <bhavyasaini06@gmail.com>
2025-03-12 00:04:15 +0530

    Day 1 adding files

---
+++ Basic_syntax/HelloWorld.java
@@ -0,0 +1,15 @@
+package Basic_syntax;
+
+public class HelloWorld {
+
+    public static void sayHello(){
+
+        System.out.println("Hello World!");
+
+    }
+
+    public static void main(String[] args){
+
+        sayHello();
+    } 
+}
